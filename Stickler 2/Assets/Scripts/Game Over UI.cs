c22202a1f378ccf8858fbad61e95653de2deb747
Emily <emilyperron2014@gmail.com>
2024-09-18 22:04:25 -0400

    Main Menu and Game Over UI

---
+++ Stickler 2/Assets/Scripts/Game Over UI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    Button restartButton;
+    Button mainMenuButton;
+    bool playerDead;
+    GameObject gameOverScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restartButton = GameObject.Find("Restart Button").GetComponent<Button>();
+        mainMenuButton = GameObject.Find("Main Menu Button").GetComponent<Button>();
+        gameOverScreen = GameObject.Find("Game Over Screen");
+        restartButton.onClick.AddListener(OnRestartButtonClick);
+        mainMenuButton.onClick.AddListener(OnMainMenuButtonClick);
+        playerDead = true;
+        gameOverScreen.gameObject.SetActive(false);
+    }
+
+    void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Waves");
+    }
+
+    void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerDead)
+        {
+            gameOverScreen.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Stickler 2/Assets/Scripts/Main Menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Button playButton;
+    Button quitButton; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton = GameObject.Find("Play Button").GetComponent<Button>();
+        quitButton = GameObject.Find("Quit Button").GetComponent<Button>();
+        playButton.onClick.AddListener(OnPlayButtonClick);
+        quitButton.onClick.AddListener(OnQuitButtonClick);
+    }
+
+    void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene("Waves");
+    }
+
+    void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
