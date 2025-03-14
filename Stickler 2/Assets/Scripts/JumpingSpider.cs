ec702755e6fb7fc0cc1e5853e1ccbcce9098f4ce
Patrick-Mayr <Patrick.mayr@ontariotechu.net>
2025-03-14 16:31:53 -0400

    made jumping spider script

---
+++ Stickler 2/Assets/Scripts/JumpingSpider.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingSpider : MonoBehaviour
+{
+    private Collider interactBox;
+    private DialogueManager dialogueManager;
+
+
+}
