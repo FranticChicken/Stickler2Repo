f393df234f23fc1579379f88473b6a64df119a81
Patrick-Mayr <Patrick.mayr@ontariotechu.net>
2025-03-18 20:57:48 -0400

    imported models and worked on tony office

--- Stickler 2/Assets/Scripts/JumpingSpider.cs
+++ Stickler 2/Assets/Scripts/JumpingSpider.cs
@@ -5,7 +5,30 @@ using UnityEngine;
 public class JumpingSpider : MonoBehaviour
 {
     private Collider interactBox;
-    private DialogueManager dialogueManager;
+    private DialogueManager dialogueBox;
+    private bool canInteract;
 
+    public void Interact()
+    {
+        if (canInteract)
+        {
+            dialogueBox.gameObject.SetActive(true);
+        }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canInteract = false;
+        }
+    }
 }
